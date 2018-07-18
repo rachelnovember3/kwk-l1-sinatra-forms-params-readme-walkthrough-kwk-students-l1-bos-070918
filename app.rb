5a5fd0320add9d0290a775ecf0a32b361cc431d4
rachelnovember3 <rachelnovember3@gmail.com>
2018-07-18 14:13:47 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -9,6 +9,7 @@ class App < Sinatra::Base
   # Add your post route and action below
   post '/food' do
     params[:fav_food]
+    "My name is #{params[:user_name]} and I love #{params[:fav_food]}."
   end
 
 end
